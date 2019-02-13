2d0b9a0797d6192a90c51d9b125bcd3cf32101ae
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2019-02-13 17:12:41 -0500

    Modify component definition by adding component name

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,5 +1,7 @@
-export default ({storeName}) => (
+const Cart = ({storeName}) => (
   <div>
     <p>This is the cart</p>
   </div>
 )
+
+export default Cart;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,20 @@
-export default ({storeName}) => (
+const MainPage = ({storeName, products}) => (
     <div>
       <p>Index page</p>
+      {storeName}
+      <ul>
+        {
+          Object.keys(products).map(key => {
+            <li key={key}>
+              <p>Hi</p>
+              {/* <h2>{products[key].name}</h2>
+              <p>{products[key].description}</p>
+              <strong>{products[key].price}</strong> */}
+            </li>
+          })
+        }
+      </ul>
     </div>
 )
+
+export default MainPage;
