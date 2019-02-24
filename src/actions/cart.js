97b730b966dca716685de9c4f2d34128861327c9
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2019-02-24 13:24:49 -0500

    Add cart and product actions

--- src/actions/cart.js
+++ src/actions/cart.js
@@ -1,3 +1,15 @@
 export const ADD_CART_ITEM = 'cart@ADD_CART_ITEM'
 export const REMOVE_CART_ITEM = 'cart@REMOVE_CART_ITEM'
 
+export const addToCart = (productId, quantity) => ({
+    type: ADD_CART_ITEM,
+    productId,
+    quantity
+})
+
+export const removeFromCart = (productId, quantity) => ({
+  type: REMOVE_CART_ITEM,
+  productId,
+  quantity
+})
+
--- src/actions/products.js
+++ src/actions/products.js
@@ -1,3 +1,20 @@
 export const ADD_PRODUCT = 'product@ADD_PRODUCT'
 export const REMOVE_PRODUCT = 'product@REMOVE_PRODUCT'
 export const UPDATE_PRODUCT = 'product@UPDATE_PRODUCT'
+
+export const addProduct = (productId, productObj) => ({
+  type: ADD_PRODUCT,
+  productId,
+  productObj
+})
+
+export const removeProduct = (productId) => ({
+  type: REMOVE_PRODUCT,
+  productId
+})
+
+export const updateProduct = (productId, productObj) => ({
+  type: UPDATE_PRODUCT,
+  productId,
+  productObj
+})
